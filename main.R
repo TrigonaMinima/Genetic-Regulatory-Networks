ad25dc0bc9edee518f842f825dc8e0421bc254eb
TrigonaMinima <shivamrana95@yahoo.in>
2015-09-02 02:27:15 +0530

    Ended GSEA!

--- main.R
+++ main.R
@@ -2,35 +2,16 @@
 # biocLite("affy")
 library(affy)
 
-# Loading the CEL files
-dat <- ReadAffy()
-dat
+# Loads all the data
+source("data.R")
 
-# Data normalization
-dat2 <- rma(dat)
-
-# If the data is large and cannot fit into computer's memory, then
-# the function justRMA() accomplishes the same task without reading
-# the data into computer's memory.
-
-# dat2 <- justRMA()
-
-# The normalized data object (dat2) obtained here is in the format internal
-# to the Bioconductor packages (affy).
-# dat2
-dat.m <- exprs(dat2)
-
-# Write the "dat.m" tabe in a text file ("affymetrix.txt")
-# using the function write.table()
-write.table(dat.m,
-    "affymetrix.txt",
-    sep="\t",
-    row.names=T,
-    col.names=T,
-    quote=F)
-
-# Create a csv
-write.csv(dat.m, "matrix.csv")
+# Scripts loading the functions to be used by the main.R
+source("std_dev_filter.R")
+source("exp_filter.R")
+source("analysis.R")
+source("gsea.R")
+source("GO.R")
+source("KEGG.R")
 
 # Source file for the quality control
 source("qc.R")
@@ -38,21 +19,35 @@ source("qc.R")
 # Removal of memory intensive objects
 rm(dat, dat2)
 
-
 # FILTERING BEFORE STATISTICAL TESTING
 # Unspecific filtering of the data
-source("std_dev_filter.R")
 dat.f <- std_filter(dat.m, "BeforeStatTest/StdFilter/")
-
-source("exp_filter.R")
 dat.fo <- exp_filter(dat.m, "BeforeStatTest/ExpFilter/")
 
 # Analysis of the data
-source("analysis.R")
 dat.s1 <- analysis(dat.f, "BeforeStatTest/StdFilter/")
 dat.s2 <- analysis(dat.fo, "BeforeStatTest/ExpFilter/")
 
-source(gsea.R)
+# Gene set enrichment analysis
+myids1 <- gsea(dat.s1, "BeforeStatTest/StdFilter/")
+myids2 <- gsea(dat.s2, "BeforeStatTest/ExpFilter/")
+
+# Analysis for GO categories
+# result1 is kinda useless. There are only 3 genes out of which
+# only 1 was recognized.
+GO_result1 <- GO(myids1, "BeforeStatTest/StdFilter/")
+GO_result2 <- GO(myids2, "BeforeStatTest/ExpFilter/")
+
+# Analysis for KEGG pathways
+# Here again, KEGG_result1 is useless.
+KEGG_result1 <- KEGG(myids1, "BeforeStatTest/StdFilter/")
+KEGG_result2 <- KEGG(myids2, "BeforeStatTest/ExpFilter/")
+
+# Gene set test
+source("gst.R")
+
+
+
 
 source("hierarchical_clust.R")
 source("kmeans.R")
@@ -61,5 +56,9 @@ source("kmeans.R")
 # FILTERING AFTER STATISTICAL TESTING
 # Unspecific filtering of the data
 dat.s <- analysis(dat.m, "AfterStatTest/")
+
 dat.s1 <- std_filter(dat.s, "AfterStatTest/StdFilter/")
 dat.s2 <- exp_filter(dat.s, "AfterStatTest/ExpFilter/")
+
+myids1 <- gsea(dat.s1dat.f, "AfterStatTest/StdFilter/")
+myids2 <- gsea(dat.s2dat.fo, "AfterStatTest/ExpFilter/")
