70c7001863ed5f71e7798e2b927ca2e2b34e16d1
TrigonaMinima <shivamrana95@yahoo.in>
2015-11-23 02:05:54 +0530

    Automation of grn.json file generation from the mappings

--- mi.py
+++ mi.py
@@ -2,89 +2,45 @@ import pandas as pd
 import numpy as np
 import itertools as it
 import json
-from collections import OrderedDict
 
 import data_read
 
-# adjacency = [[0 for x in range(10)] for x in range(10)]
 
-# orderDict = collections.OrderedDict()
+def visualization(mappings, genes, graph_file):
+    """
+    Creates json file for D3 viz in the fixed format. The format is as follows:
 
-def visualization (mappings,genes):
-    """Creates json file for visualization of the GRN by analysing the mapings dictionary"""
+    {
+        "nodes":[
+            {"name": "G1"},
+            ...
+        ],
+        "links":[
+            {"source": 0, "target": 1},
+            ...
+        ]
+    }
+    """
 
-    nod = {'nodes':[], 'links':[]}
-    # itm = collections.OrderedDict()
-    j = 0
-    i = 1
+    graph = {"nodes": [], "links": []}
 
-    for i in range(genes+1):
-        
-        if(i!=10):
-            nod["nodes"].append({})
-        nod["nodes"][i-1]["name"] = 'G' + str(i)
-        i = i + 1
+    nodes = ("G" + pd.Series(range(1, genes + 1)).map(str))
+    for gene in nodes:
+        temp = {"name": gene}
+        graph["nodes"].append(temp)
 
+    for gene in mappings:
+        for regulator in mappings[gene]:
+            temp = {"source": int(gene), "target": regulator[0]}
+            graph["links"].append(temp)
 
-    for gen, regs in mappings.items():
-        for reg in regs:
-
-            if(len(reg) == 1):
-                count = 0
-                flag = 0
-                while(count < j):
-                    if((nod['links'][count]['source'] == (reg[0] + 1)) and (nod['links'][count]['target'] == (gen + 1))):
-                        flag = 1
-                        break
-                    else:
-                        count = count + 1
-
-                if(flag == 0):
-                    nod['links'].append({})
-                    nod['links'][j]['source'] = reg[0] + 1
-                    nod['links'][j]['target'] = gen + 1
-                    j = j + 1
-                    # adjacency[reg[0]][gen] = 1
-
-            else:
-                for obj in reg:
-
-                    count = 0
-                    flag = 0
- 
-                    while(count < j):      
-                        if((nod['links'][count]['source'] == (obj + 1)) and (nod['links'][count]['target'] == (gen + 1))):
-                            flag = 1
-                            break
-                        else:
-                            count = count + 1
-
-                    if(flag == 0):
-                        nod['links'].append({})
-                        nod['links'][j]['source'] = obj + 1
-                        nod['links'][j]['target'] = gen + 1
-                        j = j + 1
-                        # adjacency[obj][gen] = 1
+    with open(graph_file, 'w') as f:
+        json.dump(graph, f, indent=4)
 
 
-    # itm = nod.items()
-    # itm.reverse()
-    # OrderedDict(itm)    
-    itm = OrderedDict(sorted(nod.items(), key=lambda t: t[0], reverse = True))
-
-    print(itm)
-    
-
-    with open('Assets/grn.json', 'w') as fp:
-        json.dump(itm, fp, indent=2, sort_keys=False)
-        # json.dump(nod, fp, indent=2, sort_keys=True)
-
-
-
-def mappings_dump(mappings):
+def mappings_dump(mappings, dump_file):
     """Dumps the mappings to a file names "mappings.json"."""
 
-    dump_file = "Data/mappings.json"
     with open(dump_file, "w") as f:
         json.dump(mappings, f, sort_keys=True, indent=4)
 
@@ -122,7 +78,6 @@ def M_analysis(dat, K):
     genes = int(len(dat.columns) / 2)
 
     for k in range(1, K):
-        
         regulators = [list(i) for i in it.combinations(range(genes), k)]
         # print(regulators)
 
@@ -131,20 +86,12 @@ def M_analysis(dat, K):
                 h1 = entropy(dat.iloc[:, regulator])
                 if h1 and h1 == entropy(dat.iloc[:, [gene + genes] + regulator]):
                     mappings[gene] = mappings.get(gene, []) + [regulator]
-            
-            
-    visualization(mappings,genes)  
 
-    # Create adjacency matrix
+    # Dumps the mappings in a file.
+    mappings_dump(mappings, "Data/mappings.json")
 
-    # i = 0
-    # while(i < 10):  # Print adjacency in martrix form
-        
-    #     print(adjacency[i])
-    #     i = i + 1
-    # #print(json.dumps(mappings, indent=4, sort_keys=True))
-
-    mappings_dump(mappings)
+    # Makes the json file for visualization.
+    visualization(mappings, genes, "Assets/grn.json")
 
 
 if __name__ == "__main__":
@@ -159,5 +106,5 @@ if __name__ == "__main__":
 
     # Performing Mutual Information on the final dataframe.
     print("MI Analysis initiated...")
-    M_analysis(final_dat, 3)
+    M_analysis(final_dat, 2)
     print("MI Analysis over! Have a look at the mappings in 'Data/mappings.json'")
