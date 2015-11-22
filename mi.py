89a497b3e9f8ebcca4f9ae13f76f99ed30be6d85
sharanagrawal <sharan.jmi@gmail.com>
2015-11-22 21:24:41 +0530

    JSON is complete but Viz is not working

--- mi.py
+++ mi.py
@@ -2,22 +2,83 @@ import pandas as pd
 import numpy as np
 import itertools as it
 import json
+from collections import OrderedDict
 
 import data_read
 
-adjacency = [[0 for x in range(10)] for x in range(10)]
+# adjacency = [[0 for x in range(10)] for x in range(10)]
 
+# orderDict = collections.OrderedDict()
 
-def admatrix(mappings):
-    """Creates adjacency matrix by analysing the mapings dictionary"""
+def visualization (mappings,genes):
+    """Creates json file for visualization of the GRN by analysing the mapings dictionary"""
+
+    nod = {'nodes':[], 'links':[]}
+    # itm = collections.OrderedDict()
+    j = 0
+    i = 1
+
+    for i in range(genes+1):
+        
+        if(i!=10):
+            nod["nodes"].append({})
+        nod["nodes"][i-1]["name"] = 'G' + str(i)
+        i = i + 1
+
 
     for gen, regs in mappings.items():
         for reg in regs:
+
             if(len(reg) == 1):
-                adjacency[reg[0]][gen] = 1
+                count = 0
+                flag = 0
+                while(count < j):
+                    if((nod['links'][count]['source'] == (reg[0] + 1)) and (nod['links'][count]['target'] == (gen + 1))):
+                        flag = 1
+                        break
+                    else:
+                        count = count + 1
+
+                if(flag == 0):
+                    nod['links'].append({})
+                    nod['links'][j]['source'] = reg[0] + 1
+                    nod['links'][j]['target'] = gen + 1
+                    j = j + 1
+                    # adjacency[reg[0]][gen] = 1
+
             else:
                 for obj in reg:
-                    adjacency[obj][gen] = 1
+
+                    count = 0
+                    flag = 0
+ 
+                    while(count < j):      
+                        if((nod['links'][count]['source'] == (obj + 1)) and (nod['links'][count]['target'] == (gen + 1))):
+                            flag = 1
+                            break
+                        else:
+                            count = count + 1
+
+                    if(flag == 0):
+                        nod['links'].append({})
+                        nod['links'][j]['source'] = obj + 1
+                        nod['links'][j]['target'] = gen + 1
+                        j = j + 1
+                        # adjacency[obj][gen] = 1
+
+
+    # itm = nod.items()
+    # itm.reverse()
+    # OrderedDict(itm)    
+    itm = OrderedDict(sorted(nod.items(), key=lambda t: t[0], reverse = True))
+
+    print(itm)
+    
+
+    with open('Assets/grn.json', 'w') as fp:
+        json.dump(itm, fp, indent=2, sort_keys=False)
+        # json.dump(nod, fp, indent=2, sort_keys=True)
+
 
 
 def mappings_dump(mappings):
@@ -61,6 +122,7 @@ def M_analysis(dat, K):
     genes = int(len(dat.columns) / 2)
 
     for k in range(1, K):
+        
         regulators = [list(i) for i in it.combinations(range(genes), k)]
         # print(regulators)
 
@@ -69,17 +131,18 @@ def M_analysis(dat, K):
                 h1 = entropy(dat.iloc[:, regulator])
                 if h1 and h1 == entropy(dat.iloc[:, [gene + genes] + regulator]):
                     mappings[gene] = mappings.get(gene, []) + [regulator]
+            
+            
+    visualization(mappings,genes)  
 
-    # print(mappings)
+    # Create adjacency matrix
 
-    # admatrix(mappings)  # Create adjacency matrix
-
-    # i = 20
-    # while(i > 10):  # Print adjacency in martrix form
-
+    # i = 0
+    # while(i < 10):  # Print adjacency in martrix form
+        
     #     print(adjacency[i])
-    #     i = i - 1
-    #print(json.dumps(mappings, indent=4, sort_keys=True))
+    #     i = i + 1
+    # #print(json.dumps(mappings, indent=4, sort_keys=True))
 
     mappings_dump(mappings)
 
